a88233d3aa8b71bb2eb59ffb1c63826a5d6c334e
Dmitry <mrdimdimk@gmail.com>
2016-11-11 21:56:07 +0300

    new

--- Omg/registCept.php
+++ Omg/registCept.php
@@ -1,6 +1,5 @@
 <?php 
 $I = new AcceptanceTester($scenario);
-
 $I->wantTo('perform actions and see result');
 $I->amOnPage('/');
 $I->click('.header-sign__auth._action',"регистрация");
@@ -13,12 +12,13 @@ $I->fillField('#user-pass',"ring");
 $I->fillField('#user-pass-repeat',"ring");
 $I->fillField('#user-tel',"2020327");
 $I->click('.register_button.register_button.js-submitBtn');
+$I->waitForElement('b-modal__window', 5);
 $I->dontSee('.b-modal__window');
 $I->see("Фродо",'.header-sign__login');
 $I->click('.header-sign__login');
 $I->click('.login-nav__link');
-$I->seeInCurrentUrl('http://test.lama.by.dev.lovata.com/user/update/78');
-$I->see("Фродо",'.register-block__name');
-$I->see("b.bagind@shire.com",'.register-block__field-email');
+$I->see('input.user-info__field-name[value="Фродо"]');
+$I->see('input.user-info__field-email[value="b.bagind@shire.com"]');
 $I->see("Ваши работы",'.user__switch _switch-active');
 $I->see("Сохранить",'save_button js-submitBtn');
+$I->seeInCurrentUrl('http://test.lama.by.dev.lovata.com/user/update/78');
